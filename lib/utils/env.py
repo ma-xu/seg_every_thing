6b25aee27c11f51e72e006eb1639a3d715b6b5d0
13952522076 <xuma@my.unt.edu>
2020-06-29 19:38:52 -0500

    update

--- lib/utils/env.py
+++ lib/utils/env.py
@@ -60,6 +60,7 @@ def get_detectron_ops_lib():
     """Retrieve Detectron ops library."""
     # Candidate prefixes for the detectron ops lib path
     prefixes = [_CMAKE_INSTALL_PREFIX, sys.prefix, sys.exec_prefix] + sys.path
+    print("prefixes: {}".format(prefixes))
     # Search for detectron ops lib
     for prefix in prefixes:
         ops_path = os.path.join(prefix, 'lib/libcaffe2_detectron_ops_gpu.so')
